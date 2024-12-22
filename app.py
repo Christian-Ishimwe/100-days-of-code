9691ac1c297d3ee99554d5080551964f392aa2e0
christian ISHIMWE <christianinja3@gmail.com>
2024-12-22 11:36:33 +0200

    First commit

--- app.py
+++ app.py
@@ -1,9 +1,1 @@
-# 1year= 52 years
-# 1year= 365 days
-# 1year= 12 months
-# total years= 90 years
-
-age= int(input("What is your current age? "))
-remaining_ages= 90-age
-print(f'Your have {remaining_ages*365} days, {remaining_ages*52} weeks, and {remaining_ages*12} months left.')
-
+print("Welcome to the 100 days of code for Python course!")
--- project.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to the tip calculator.")
-total_bill= float(input("What was the total  bill? $"))
-percent= int(input("What percentage tip would you like to give? 10,12, or 15? "))
-number_people= int(input("How many people to split the bill? "))
-print(f"Each person should pay: ${round((total_bill/number_people)*(1+percent/100), 2)}")
